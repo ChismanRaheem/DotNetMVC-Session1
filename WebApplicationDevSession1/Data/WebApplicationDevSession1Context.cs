608ac8c2998930de5565dd04fa4c49496e3428d9
Raheem <159470974+ChismanRaheem@users.noreply.github.com>
2024-07-02 23:51:27 -0700

    Add project files.

---
+++ WebApplicationDevSession1/Data/WebApplicationDevSession1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationDevSession1.Models;
+
+namespace WebApplicationDevSession1.Data
+{
+    public class WebApplicationDevSession1Context : DbContext
+    {
+        public WebApplicationDevSession1Context (DbContextOptions<WebApplicationDevSession1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationDevSession1.Models.ToDoItem> ToDoItem { get; set; } = default!;
+    }
+}
